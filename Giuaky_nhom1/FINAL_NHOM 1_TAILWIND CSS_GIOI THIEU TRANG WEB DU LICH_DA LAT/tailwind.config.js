953fd7feb5c0c31a56933a7ed822501a035f1457
quydoho <2211196@dlu.edu.vn>
2025-05-05 15:18:59 +0700

    initial commit

---
+++ Giuaky_nhom1/FINAL_NHOM 1_TAILWIND CSS_GIOI THIEU TRANG WEB DU LICH_DA LAT/tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './src/pages/**/*.html',
+    './src/js/**/*.js'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        times: ['"Times New Roman"', 'Times', 'serif']
+      }
+    },
+  },
+  plugins: [],
+}
